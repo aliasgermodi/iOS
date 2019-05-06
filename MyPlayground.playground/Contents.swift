735b7929ce235b2f689a1eddf0b5c6d8367e8167
aliasgermodi <modialiasger@gmail.com>
2019-05-06 14:22:15 +0300

    First commit for playground

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+//This is a comment
+var str:String = "Hello, playground"
+print(str)
+str = "Sample Data"
+print(str)
+
+var b:Bool = true
+print(b)
+
+var i:Int = 30
+
+i = 0
+i = -10
+
+
+var f:Double = 0.3456
